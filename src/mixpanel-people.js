85be06c98e5f1a29b9ef501c7700bfe7067af39f
Nir Elbaz <nire0510@gmail.com>
2015-07-16 12:29:34 +0300

    modules refactoring

---
+++ src/mixpanel-people.js
@@ -0,0 +1,23 @@
+/* global module, require */
+module.exports = {
+  identifyUser: identifyUser,
+  setUserProperties: setUserProperties
+};
+
+/**
+ * Identify a user with a unique ID. All subsequent actions caused by this user will be tied to this unique ID
+ * @param {string} strIdentity A string that uniquely identifies a user
+ */
+function identifyUser (strIdentity) {
+  mixpanel.identify(strIdentity);
+}
+
+/**
+ * Set properties on a user record
+ * @param {string|object} varProperty If a string, this is the name of the property. If an object, this is an associative array of names and values
+ * @param {*} [varValue] A value to set on the given property name
+ * @param {function} [fncCallback] If provided, the callback will be called after the tracking event
+ */
+function setUserProperties (varProperty, varValue, fncCallback) {
+  mixpanel.people.set(varProperty, varValue, fncCallback);
+}
---
+++ src/mixpanel-tracking.js
@@ -0,0 +1,99 @@
+/* global module, require */
+var config = require('./config.js');
+
+module.exports = {
+  disableEvents: disableEvents,
+  trackEvent: trackEvent,
+  trackPageView: trackPageView,
+  trackLink: trackLink,
+  trackForm: trackForm,
+  trackCharge: trackCharge,
+  registerProperties: registerProperties,
+  unregisterProperties: unregisterProperties
+};
+
+/**
+ * Disable events on the Mixpanel object
+ * @param {[string]} [arrEventNames] An array of event names to disable. If passed no arguments, this function disables tracking of any event.
+ */
+function disableEvents (arrEventNames) {
+  mixpanel.disable(arrEventNames);
+}
+
+/**
+ * Track an event
+ * @param {string} strEventName The name of the event
+ * @param {object} [objProperties] A set of properties to include with the event you're sending
+ * @param {function} [fncCallback] If provided, the callback function will be called after tracking the event
+ */
+function trackEvent (strEventName, objProperties, fncCallback) {
+  mixpanel.track(strEventName || config.dictionary.missingEventName, objProperties || {}, fncCallback);
+}
+
+/**
+ * Track page viewed event
+ * @param {object} [objProperties] A set of properties to include with the event you're sending
+ */
+function trackPageView (objProperties) {
+  // Works only in browser environment:
+  if (this === window) {
+    objProperties = objProperties || {};
+    objProperties[config.dictionary.pageNamePropertyName] = document.title;
+    objProperties[config.dictionary.pageURLPropertyName] = window.location.pathname;
+
+    this.track(config.dictionary.pageViewedEventName, objProperties);
+  }
+}
+
+/**
+ * Track link click event
+ * @param {string} strSelector A valid DOM query selector
+ * @param {object} strEventName The name of the event to track
+ * @param {object} [objProperties] A properties object or function that returns a dictionary of properties when passed a DOMElement
+ */
+function trackLink (strSelector, strEventName, objProperties) {
+  // jscs:disable requireCamelCaseOrUpperCaseIdentifiers
+  mixpanel.track_links(strSelector, strEventName, objProperties);
+  // jscs:enable requireCamelCaseOrUpperCaseIdentifiers
+}
+
+/**
+ * Track form submission event
+ * @param {string} strSelector A valid DOM query
+ * @param {object} strEventName The name of the event to track
+ * @param {object} [objProperties] A properties object or function that returns a dictionary of properties when passed a DOMElement
+ */
+function trackForm (strSelector, strEventName, objProperties) {
+  // jscs:disable requireCamelCaseOrUpperCaseIdentifiers
+  mixpanel.track_forms(strSelector, strEventName, objProperties);
+  // jscs:enable requireCamelCaseOrUpperCaseIdentifiers
+}
+
+/**
+ * Record that you have charged the current user a certain amount of money
+ * @param {number} fltAmount The amount of money charged to the current
+ * @param {object} [objProperties] An associative array of properties associated with the charge
+ * @param {function} [fncCallback] If provided, the callback will be called when the server responds
+ */
+function trackCharge (fltAmount, objProperties, fncCallback) {
+  // jscs:disable requireCamelCaseOrUpperCaseIdentifiers
+  mixpanel.people.track_charge(fltAmount, objProperties, fncCallback);
+  // jscs:enable requireCamelCaseOrUpperCaseIdentifiers
+}
+
+/**
+ * Register a set of super properties, which are included with all events
+ * @param {object} objProperties An associative array of properties to store about the user
+ * @param {number} [intDays] How many days since the user's last visit to store the super properties
+ */
+function registerProperties (objProperties, intDays) {
+  mixpanel.register(objProperties, intDays);
+}
+
+/**
+ * Delete a super property stored with the current user
+ * @param {string} strProperty The name of the super property to remove
+ */
+function unregisterProperties (strProperty) {
+  mixpanel.unregister(strProperty);
+}
